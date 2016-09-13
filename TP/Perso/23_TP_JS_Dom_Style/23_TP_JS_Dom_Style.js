8052e5840ac38f574f28e910c6e30988443fd6e7
Flaka <gb2get@gmail.com>
2016-09-13 16:28:36 +0200

    TP

--- TP/Perso/23_TP_JS_Dom_Style/23_TP_JS_Dom_Style.js
+++ TP/Perso/23_TP_JS_Dom_Style/23_TP_JS_Dom_Style.js
@@ -1,6 +1,5 @@
 function press()
 {
-	var baliseDiv = document.getElementById("idDivImage");
 	var baliseImg = document.getElementById("idImage");
 	baliseImg.setAttribute("src", "images/shadow-proof.jpg");
 	var preuve = document.getElementById("idDivPreuve");
@@ -10,7 +9,6 @@ function press()
 
 function depress()
 {
-	var baliseDiv = document.getElementById("idDivImage");
 	var baliseImg = document.getElementById("idImage");
 	baliseImg.setAttribute("src", "images/shadow-illusion.jpg");
 	var preuve = document.getElementById("idDivPreuve");
