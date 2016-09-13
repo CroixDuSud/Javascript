10fc8d27773731fb468a8a29c8074f250b6ab683
Flaka <gb2get@gmail.com>
2016-09-13 16:00:51 +0200

    disposition

---
+++ TP/Perso/23_TP_JS_Dom_Style/23_TP_JS_Dom_Style.js
@@ -0,0 +1,11 @@
+function press()
+{
+	var a = document.getElementById("idDivImage");
+	var b = document.getElementById("idImage")
+	document.getElementById(a).removeChild(b);
+	//document.getElementById("idDivImage").innerHTML ="";
+/*
+	document.getElementById("idImage").innerHTML = 
+	"<img id="idImage2" src="images/shadow-proof.jpg" alt="Preuvre">";
+*/
+}
